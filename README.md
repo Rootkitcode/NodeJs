dcdba967534b83cce5f692841f157a96d7abbc55
Rootkitcode <coaldar85@gmail.com>
2022-03-18 14:12:36 -0500

    all example endpoints

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NodeJs
 This project is an example about an API of Movies.
 remember run in your console npm install for build the  nodejs modules.
+
+On this example I don't use any database this is only example of how we can build an api with nodejs without database.
+
+Also on this example I builded an examble.json that simulate a database.
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const router = Router();
-
+const underscore = require('underscore');
 const movies = require('../xample.json');
 
 router.get('/', (req, res) => {
@@ -17,6 +17,34 @@ router.post('/', (req, res) => {
     }else{
         res.json({'message': 'error, missing data'});
     }
+});
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const {title, year, genres, raitings} = req.body;
+    if(title && year && genres && raitings){
+        underscore.each(movies,(movie, index) => {
+            if(movies.id == id){
+                movie.title = title;
+                movie.year = year;
+                movie.genres = genres;
+                movie.raitings = raitings;
+            }
+        });
+        res.json({'message': 'ok, movie updated'});
+    } else {    
+        res.json({'message': 'error, missing data'});
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    underscore.each(movies, (movie, index) => {
+        if(movie.id === parseInt(req.params.id)){
+            movies.splice(index, 1);
+        }
+    });
+    res.json({'message': 'ok, movie deleted'});
 });
 
 module.exports = router;
